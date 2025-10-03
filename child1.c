3d3be38100d5ac3451a47c6e21d444d1e0f7b9e8
adios74 <sonya.rozhkova@gmail.com>
2025-10-04 02:02:13 +0300

    Add files via upload

---
+++ child1.c
@@ -0,0 +1,54 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+
+void invertation(char* stroka, int length) {
+    char* start = stroka;
+    char* end = stroka + length - 1;
+    while(start < end) {
+        char a = *start;
+        *start = *end;
+        *end = a;
+        start++;
+        end--;
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        const char msg[] = "Wrong number of arguments\n";
+        write(STDERR_FILENO, msg, sizeof(msg));
+        exit(EXIT_FAILURE);
+    }
+
+    const char* filename = argv[1];
+    const char* data = argv[2];
+    int len = strlen(data);
+
+    int file = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+    if (file == -1) {
+        const char msg[] = "error: failed to open file 1\n";
+        write(STDERR_FILENO, msg, sizeof(msg));
+        exit(EXIT_FAILURE);
+    }
+
+    char* inverted = malloc(len + 1);
+    strcpy(inverted, data);
+    invertation(inverted, len);
+
+    write(file, inverted, len);
+    write(file, "\n", 1);
+
+    const char ans[] = "Result №1: ";
+    write(STDOUT_FILENO, ans, sizeof(ans) - 1);
+    write(STDOUT_FILENO, inverted, len);
+    write(STDOUT_FILENO, "\n", 1);
+
+    close(file);
+    free(inverted);
+    return 0;
+}
